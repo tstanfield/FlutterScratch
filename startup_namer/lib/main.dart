47947ecec602428a34531614fb9c46f4efdc6fb6
Terry Stanfield <tstanfield@hylaine.com>
2021-08-17 13:33:41 -0400

    step 5 Add a stateful widget

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -14,9 +14,22 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
