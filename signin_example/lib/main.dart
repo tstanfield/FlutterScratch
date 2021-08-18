49131a8cdce0eb2ed9a1851c7768404c3f57084e
Terry Stanfield <tstanfield@hylaine.com>
2021-08-18 11:24:18 -0400

    added form field validation

--- signin_example/lib/main.dart
+++ signin_example/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(SignUpApp());
 
 class SignUpApp extends StatelessWidget {
+  const SignUpApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,31 +33,42 @@ class SignUpScreen extends StatelessWidget {
 }
 
 class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
   @override
   _SignUpFormState createState() => _SignUpFormState();
 }
 
-class _SignUpFormState extends State<SignUpForm> {
+class _SignUpFormState extends State<SignUpForm> with InputValidationMixin {
   final _firstNameTextController = TextEditingController();
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
   double _formProgress = 0;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          LinearProgressIndicator(value: _formProgress),
+          AnimatedProgressIndicator(value: _formProgress),
           Text('Sign up', style: Theme.of(context).textTheme.headline4),
           Padding(
             padding: EdgeInsets.all(8.0),
             child: TextFormField(
               controller: _firstNameTextController,
               decoration: InputDecoration(hintText: 'First name'),
+              validator: (firstName) {
+                if (isNameValid(firstName))
+                  return null;
+                else
+                  return "First name must be more than 2 characters";
+              },
             ),
           ),
           Padding(
@@ -64,6 +76,12 @@ class _SignUpFormState extends State<SignUpForm> {
             child: TextFormField(
               controller: _lastNameTextController,
               decoration: InputDecoration(hintText: 'Last name'),
+              validator: (lastName) {
+                if (isNameValid(lastName))
+                  return null;
+                else
+                  return "Last name must be more than 2 characters";
+              },
             ),
           ),
           Padding(
@@ -71,6 +89,12 @@ class _SignUpFormState extends State<SignUpForm> {
             child: TextFormField(
               controller: _usernameTextController,
               decoration: InputDecoration(hintText: 'Username'),
+              validator: (userName) {
+                if (isEmailValid(userName))
+                  return null;
+                else
+                  return "Invalid email address";
+              },
             ),
           ),
           TextButton(
@@ -88,8 +112,14 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed:
-                _formProgress == 1 ? _showWelcomeScreen : null, // UPDATED
+            //onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
+            onPressed: () {
+              if (_formKey.currentState!.validate() && _formProgress == 1) {
+                _formKey.currentState?.save();
+                return _showWelcomeScreen();
+              }
+              return null;
+            },
             child: Text('Sign up'),
           ),
         ],
@@ -129,5 +159,84 @@ class WelcomeScreen extends StatelessWidget {
         child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
       ),
     );
+  }
+}
+
+class AnimatedProgressIndicator extends StatefulWidget {
+  final double value;
+
+  AnimatedProgressIndicator({
+    required this.value,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnimatedProgressIndicatorState();
+  }
+}
+
+class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double> _curveAnimation;
+
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: Duration(milliseconds: 1200), vsync: this);
+
+    final colorTween = TweenSequence([
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.red, end: Colors.orange),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.yellow, end: Colors.green),
+        weight: 1,
+      ),
+    ]);
+
+    _colorAnimation = _controller.drive(colorTween);
+    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
+  }
+
+  @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.animateTo(widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) => LinearProgressIndicator(
+        value: _curveAnimation.value,
+        valueColor: _colorAnimation,
+        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
+      ),
+    );
+  }
+}
+
+mixin InputValidationMixin {
+  bool isPasswordValid(String? password) => password?.length == 6;
+
+  bool isNameValid(String? name) {
+    if (name == null) return false;
+    return name.length >= 2;
+  }
+
+  bool isEmailValid(String? email) {
+    if (email == null) return false;
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    return regex.hasMatch(email);
   }
 }
